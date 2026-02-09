49160b95831ca6751b3f1af82855424f6a43ad70
AlainDLC <jd_alain@hotmail.com>
2026-02-09 21:00:28 +0100

    fics

--- ykb-tech/middleware.js
+++ ykb-tech/middleware.js
@@ -1,3 +1,5 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
 const isDashboardRoute = createRouteMatcher(["/dashboard(.*)"]);
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
@@ -13,7 +15,7 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
