8b96cae024bffc70c5b0002a3446c8cd5ea501e6
AlainDLC <jd_alain@hotmail.com>
2026-02-20 07:10:11 +0100

    change homepage

--- ykb-tech/app/page.js
+++ ykb-tech/app/page.js
@@ -370,7 +370,7 @@ export default function HomePage() {
                   <Truck size={18} className="text-white" />
                 </div>
                 <span className="font-black italic uppercase tracking-tighter text-xl text-slate-900">
-                  YKB
+                  ADR YKB Yrkestrafiktillstånd
                 </span>
               </div>
               <p className="text-4xl lg:text-5xl font-black uppercase italic tracking-tighter leading-[0.85] text-slate-200">
